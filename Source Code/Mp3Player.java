7f8af76f522bea1e6152ffa176598dc2a959b657
Katarina Capalbo <kvc750@gmail.com>
2019-07-15 15:48:16 -0400

    Update Mp3Player.java

--- Source Code/Mp3Player.java
+++ Source Code/Mp3Player.java
@@ -1,5 +1,6 @@
 /*
- * Name:		Katarina Capalbo n01399315
+ * Name:		Katarina Capalbo
+ * Student ID:  n01399315
  * Date:		5/28/2019
  * Assignment:	Project 1 COP3530
  * 
@@ -23,7 +24,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
+// start class MP3Player
 public class Mp3Player extends JPanel implements ActionListener{
+    // Variable Declarations
     static JButton play,pause,next;
     JLabel songname, songlist;
     PlayThread runnable;
@@ -31,6 +34,7 @@ public class Mp3Player extends JPanel implements ActionListener{
     static List songList;
     static int selectedSong;
     
+    // Constructor
     public Mp3Player() {
         selectedSong = 0;
         //GridLayout layout = new GridLayout(0,3);
@@ -67,8 +71,9 @@ public class Mp3Player extends JPanel implements ActionListener{
         add(pause);
         add(next);
         add(songlist);
-    }
+    }// end constructor
     
+    // actionPerformed() sets up controls for the "play", "pause", and "next" buttons
     public void actionPerformed(ActionEvent e) {
         if("play".equals(e.getActionCommand())) {
             runnable = new PlayThread(songList.get(selectedSong));
@@ -89,12 +94,16 @@ public class Mp3Player extends JPanel implements ActionListener{
             String nextsong = songList.get(selectedSong);
             songname.setText(nextsong);
         }
-    }
+    }// end actionPerformed(ActionEvent e)
+    
+    // doneSong() sets the end of the song
     public static void doneSong() {
         
         pause.setEnabled(false);
         play.setEnabled(true);
-    }
+    } // end doneSong()
+    
+    // createAndShowGUI() sets up the frame and content pane for the MP3 GUI
     private static void createAndShowGUI() {
         
         //Create and set up the window.
@@ -109,8 +118,9 @@ public class Mp3Player extends JPanel implements ActionListener{
         //Display the window.
         frame.pack();
         frame.setVisible(true);
-    }
+    }// end createAndShowGUI()
     
+    //start main()
     public static void main(String[] args) {
         songList = new List();
         songList.insertItem("barbie_girl.mp3");
@@ -137,14 +147,19 @@ public class Mp3Player extends JPanel implements ActionListener{
         });
         
     }
-}
+}// end main()
 
+// start class PlayThread
 class PlayThread implements Runnable {
+    // Variable Declarations
     public String song;
     
+    // constructor
     public PlayThread(String song) {
         this.song = song;
-    }
+    }// end constructor
+    
+    // run() sets up the file input stream to run the .mp3 files
     public void run() {
         try{
             FileInputStream fis = new FileInputStream(song);
@@ -152,7 +167,7 @@ class PlayThread implements Runnable {
             p.play();
         } catch(Exception e){}
         Mp3Player.doneSong();
-    }
+    }// end run()
     
-}
+}// end class PlayThreat
 
